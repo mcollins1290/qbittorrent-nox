7de7b7d564040e1e4589d16e9a48ed574db09969
Martin Collins <mcollins1290@gmail.com>
2023-02-23 17:41:28 -0500

    Revised GCC compiler flags for RPi4

--- qbittorrent-nox-static.sh
+++ qbittorrent-nox-static.sh
@@ -210,8 +210,8 @@ custom_flags_set() {
 	CC=arm-linux-gnueabihf-gcc
 	CXX=arm-linux-gnueabihf-g++
 
-	CXXFLAGS="-std=${standard} -mcpu=cortex-a72 -mfloat-abi=hard -mfpu=neon-vfpv4 -mtune=cortex-a72 -I${include_dir}"
-	CPPFLAGS="--static -static -mcpu=cortex-a72 -mfloat-abi=hard -mfpu=neon-vfpv4 -mtune=cortex-a72 -I${include_dir}"
+	CXXFLAGS="-std=${standard} -march=armv8-a -mfpu=neon-fp-armv8 -mcpu=cortex-a72 -mfloat-abi=hard -mtune=cortex-a72 -I${include_dir}"
+	CPPFLAGS="--static -static -march=armv8-a -mfpu=neon-fp-armv8 -mcpu=cortex-a72 -mfloat-abi=hard -mtune=cortex-a72 -I${include_dir}"
 	LDFLAGS="--static -static -Wl,--no-as-needed -L${lib_dir} -lpthread -pthread"
 }
 custom_flags_unset() {
